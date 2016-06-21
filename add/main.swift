48fe35c65a667437475ddd89c245f2e3efdb88e0
13604786021 <CIEC_iMac@CIEC-iMacs-iMac.local>
2016-06-21 09:41:13 +0800

    swfit练手之冒泡排序
    将传统的冒泡排序用swift语言写出来，作用是体验循环，选择和数组在swift语言中的用法

--- add/main.swift
+++ add/main.swift
@@ -8,8 +8,21 @@
 
 import Foundation
 
-var vall=0
-for var i=0;i<=100;++i{
-vall=vall+i
+var b=[2,23,12,1.5,4,43543,3,32.4,232,44556,23.5,29,11,78,989,45,0.177,12.7,78.5,46]
+var temp:Double = 0
+for var i=0;i<20-1;i++
+{
+    for var j=0;j<20-1-i;j++
+    {
+        if b[j]>b[j+1]
+        {
+            temp=b[j];
+            b[j]=b[j+1];
+            b[j+1]=temp;
+        }
+    }
 }
-print(vall)
+for var i=0;i<20;i++
+{
+    print(b[i])
+}
